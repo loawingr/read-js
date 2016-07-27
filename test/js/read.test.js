b14ae6ee374a68a895a6e40f18490fde6ca47f1b
Richard Loa <loawinr@gmail.com>
2016-07-27 00:00:46 -0400

    adding safeguards on the read js config

--- test/js/read.test.js
+++ test/js/read.test.js
@@ -24,6 +24,14 @@ describe("read-js-tests", function(){
         expect(window.readJS.getText(el)).toBe("Hello there! The text of the dom node should  be returned");
     });
 
+    it("config", function(){
+        expect(window.readJS.timeInterval).toBeGreaterThan(1); //for performance reasons
+        expect(window.readJS.thresholds.readingPoint).toBeGreaterThan(window.readJS.activity.increment);
+        expect(window.readJS.thresholds.domPolling).toBe(window.readJS.activity.increment);
+        expect(1 < window.readJS.thresholds.viewport && window.readJS.thresholds.viewport <= 100).toBeTruthy();
+        expect(1 < window.readJS.thresholds.domNode && window.readJS.thresholds.domNode <= 100).toBeTruthy();
+    });
+
     //do something after each test
     afterEach(function() {
     });
