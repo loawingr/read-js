9145d69590c79725c268d90ebb063bba4fd42ff6
Richard Loa <richard.loa@cbc.ca>
2017-02-12 20:16:27 -0500

    only set debug flags to true if they haven't already been defined

--- src/js/config.js
+++ src/js/config.js
@@ -4,7 +4,12 @@ if (!readJSConfig.cb){ readJSConfig.cb = function(){
     "use strict";
     alert("The article has been read");
 }; }
-readJSConfig.debug = {
-	overlay: true,
-	console: true
-};
+if (!readJSConfig.debug){
+	readJSConfig.debug = {};
+	if (typeof(readJSConfig.debug.overlay)==="undefined"){
+		readJSConfig.debug.overlay = true;
+	}
+	if (typeof(readJSConfig.debug.console)==="undefined"){
+		readJSConfig.debug.console = true;
+	}
+}
