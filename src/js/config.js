5ed8ebda6e4b4e220fcc0718bdc30c0445749490
David Viscomi <david.viscomi@cbc.ca>
2018-07-25 17:53:07 -0400

    adding in eslint build and configuring js files with changes

--- src/js/config.js
+++ src/js/config.js
@@ -1,15 +1,17 @@
 var readJSConfig = window.readJSConfig || {};
-if (!readJSConfig.el){ readJSConfig.el = ".story-body"; }
-if (!readJSConfig.cb){ readJSConfig.cb = function(){
-    "use strict";
-    alert("The article has been read");
-}; }
-if (!readJSConfig.debug){
-	readJSConfig.debug = {};
-	if (typeof(readJSConfig.debug.overlay)==="undefined"){
-		readJSConfig.debug.overlay = true;
-	}
-	if (typeof(readJSConfig.debug.console)==="undefined"){
-		readJSConfig.debug.console = true;
-	}
+if (!readJSConfig.el) { readJSConfig.el = ".story-body"; }
+if (!readJSConfig.cb) {
+    readJSConfig.cb = function() {
+        "use strict";
+        alert("The article has been read");
+    };
+}
+if (!readJSConfig.debug) {
+    readJSConfig.debug = {};
+    if (typeof(readJSConfig.debug.overlay) === "undefined") {
+        readJSConfig.debug.overlay = true;
+    }
+    if (typeof(readJSConfig.debug.console) === "undefined") {
+        readJSConfig.debug.console = true;
+    }
 }
