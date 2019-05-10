065c28fc36e6e8bdb5cb0347baf3a0359b9bec82
Man, Bliss <bliss.man@bellmedia.ca>
2019-05-10 11:38:59 -0400

    run npm audit fix for insecure packages, add test cases for googlebot detection

--- test/js/spa.test.js
+++ test/js/spa.test.js
@@ -13,6 +13,17 @@ describe("spa tests of readJS", function(){
         expect(readJS.isOn()).toBeFalsy();
     });
 
+    it("should return false if the user agent contains \"Googlebot\"", function() {
+        expect(readJS.checkGooglebot("Googlebot-Image/1.0")).toBeFalsy();
+        expect(readJS.checkGooglebot("Googlebot-News")).toBeFalsy();
+        expect(readJS.checkGooglebot("Googlebot-Video/1.0")).toBeFalsy();
+        expect(readJS.checkGooglebot("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")).toBeFalsy();
+        expect(readJS.checkGooglebot("Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Safari/537.36")).toBeFalsy();
+        expect(readJS.checkGooglebot("Googlebot/2.1 (+http://www.google.com/bot.html)")).toBeFalsy();
+        expect(readJS.checkGooglebot("Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")).toBeFalsy();
+        expect(readJS.checkGooglebot("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36")).toBeTruthy();
+    });
+
     it("should not be turned on", function(){
         //can't turn on because missing el and cb
         expect(readJS.turnOn()).toBeFalsy();
